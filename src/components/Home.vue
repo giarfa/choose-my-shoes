a3d8be373a61606b8a18aed22927deaba8b876e4
Francesco Giarola <giarfa@outlook.com>
2021-02-27 17:24:27 +0100

    t

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/sneakers.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
